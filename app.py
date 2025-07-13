40285dca66fd47d6abbae894657bd0a743a2a738
aashishrrjp <aashishrrjp@google.com>
2025-07-13 16:10:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask running in Kubernetes with Argo CD!"
+    return "Hello from Flask running in Kubernetes with Argo CD and i am using GCP!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
